d04e8594a73070554ca0c88e2856c779c7a62898
19시_웹개발_KDH <19__KDH@DESKTOP-HL81TMQ>
2021-03-25 20:26:50 +0900

    2021.03.25

---
+++ 09_java_OOP_technology/src/step9_01/OOP_Theory/OOPEX07_02.java
@@ -0,0 +1,47 @@
+package step9_01.OOP_Theory;
+
+public class OOPEX07_02 implements OOPEx07_01 {
+
+	@Override
+	public void showdata1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showdata2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showdata3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showdata4() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showdata5() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showdata6() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showdata7() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ 09_java_OOP_technology/src/step9_01/OOP_Theory/OOPEx07_01.java
@@ -0,0 +1,13 @@
+package step9_01.OOP_Theory;
+
+public interface OOPEx07_01 {
+
+	void showdata1();
+	void showdata2();
+	void showdata3();
+	void showdata4();
+	void showdata5();
+	void showdata6();
+	void showdata7();
+
+}
